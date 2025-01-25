26116b6c929c45b6e3620070b52f6f76f1b36987
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-25 23:46:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-sky-600 to-gray-100 font-sans">
-      <Home />
+      <Home/>
     </div>
   );
 }
