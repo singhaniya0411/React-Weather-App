f9ca9ae973f4c48fc4eed429593359a2fc38af9c
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-25 23:47:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 
+
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-sky-600 to-gray-100 font-sans">
