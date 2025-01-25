35610049d7a725a19ba3ef627125df8b725d5d32
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-25 23:49:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,10 @@ import App from "./App";
 import "./index.css";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    
     <AppProvider>
       <App />
     </AppProvider>
+    
   </BrowserRouter>
 );
