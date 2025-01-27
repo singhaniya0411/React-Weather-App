159e7734d79167b6e8e54722b1f34e8523922694
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-27 15:29:58 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,23 @@ export default {
   ],
   theme: {
     extend: {
-
+      // You can still extend other utilities here, but textShadow goes in a plugin
     },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow-sm': {
+          textShadow: '1px 1px 2px rgba(17, 179, 250, 0.5)',
+        },
+        '.text-shadow': {
+          textShadow: '2px 2px 4px #fff',
+        },
+        '.text-shadow-lg': {
+          textShadow: '1px 1px 0px #fff',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+};
