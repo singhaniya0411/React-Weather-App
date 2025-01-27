e601a925b264384cbdb06692935d0a345ce5895e
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-27 15:28:34 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,6 +31,7 @@ const Home = () => {
     Squall: assets.mist,
     Tornado: assets.mist,
     Fog: assets.mist,
+    WeatherIcon: assets.weatherImg,
   };
 
   const isNight =
@@ -45,25 +46,47 @@ const Home = () => {
     weatherMap["Clear"] = assets.moon;
   }
   const weatherImage = weatherMap[weather];
+  const Weathorlogo = weatherMap["WeatherIcon"];
   return (
-    <div
-      className={`max-w-md mx-auto h-auto ${
-        darkMode
-          ? "bg-gradient-to-r from-gray-500 to-gray-300 border-gray-950"
-          : " bg-white border-blue-300 "
-      }  rounded-2xl border p-2 my-1 sm:h-[720px] md:max-w-lg lg:max-w-xl lg:h-auto`}
-    >
-      <WeatherSearch />
-      <div className="mt-8 flex justify-center">
-        <div className="relative w-fit">
-          <img className="w-32  mix-blend-multiply" src={weatherImage} />
+    <div className="flex flex-col gap-4">
+      <div
+        className={`top-4 flex gap-4 mb-2 mt-5 items-center ${
+          darkMode
+            ? "bg-gradient-to-r from-gray-500 to-gray-200 border-black border"
+            : "bg-white "
+        } pl-6 py-3 pr-6 rounded-md justify-between`}
+      >
+        <div
+          className={`text-2xl   ${
+            darkMode
+              ? "text-black text-shadow-lg"
+              : "bg-gradient-to-r from-blue-500 to-blue-300 bg-clip-text text-transparent text-shadow-sm"
+          }`}
+        >
+          WeatherSphere
         </div>
+        <img className="w-10 mix-blend-multiply" src={Weathorlogo} alt="" />
       </div>
-      <div className="mt-4 text-center font-semibold text-lg sm:text-xl lg:text-2xl">
-        {weather}
-      </div>
-      <div className="py-4">
-        <Details />
+
+      <div
+        className={`max-w-md mx-auto h-auto ${
+          darkMode
+            ? "bg-gradient-to-r from-gray-500 to-gray-300 border-gray-950"
+            : " bg-white border-blue-300 "
+        }  rounded-2xl border p-2 my-1 sm:h-[720px] md:max-w-lg lg:max-w-xl lg:h-auto mt-2 mb-24`}
+      >
+        <WeatherSearch />
+        <div className="mt-8 flex justify-center">
+          <div className="relative w-fit">
+            <img className="w-32  mix-blend-multiply" src={weatherImage} />
+          </div>
+        </div>
+        <div className="mt-4 text-center font-semibold text-lg sm:text-xl lg:text-2xl">
+          {weather}
+        </div>
+        <div className="py-4">
+          <Details />
+        </div>
       </div>
     </div>
   );
